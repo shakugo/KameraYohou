79c2f5a12d2a720f5de1ff0507696571ab1a9795
takumi34 <1shiranui@gmail.com>
2019-12-06 00:05:06 +0900

    update register.dart

--- lib/register.dart
+++ lib/register.dart
@@ -1,55 +1,58 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  bool _isChecked = false;
+
+// TODO 今は決め打ちでリストの中身が入ってるので可変にする
+  List<KeyValueFavorites> _datas = [
+    KeyValueFavorites(key: "桜", isChecked: false),
+    KeyValueFavorites(key: "バラ", isChecked: false),
+    KeyValueFavorites(key: "菊", isChecked: false),
+    KeyValueFavorites(key: "紅葉", isChecked: false),
+
+  ];
+
+
+
+  List<String> _texts = [
+    "桜","菊","バラ","ビル",
+  ];
+
+
+  //TODO とりあえず今は_textsで表示している。あとで_datasに差し替え。現状の_textsだとkeyがないので、checkboxが全て連動している状態
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Form',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Form'),
-        ),
-        body: Center(
-          child: ChangeForm(),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Favarites List"),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: _texts.map((text) => CheckboxListTile(
+          title: Text(text),
+          value: _isChecked,
+          onChanged: (val) {
+            setState(() {
+              _isChecked = val;
+
+            });
+          },
+        )).toList(),
       ),
     );
   }
 }
 
-class ChangeForm extends StatefulWidget {
-  @override
-  _ChangeFormState createState() => _ChangeFormState();
-}
+// favoritesのリストにそれぞれキー、バリューを持たせるため
+class KeyValueFavorites {
+  String key;
+  bool isChecked;
 
-class _ChangeFormState extends State<ChangeForm> {
-  bool _flag = false;
-
-  void _handleCheckbox(bool e) {
-    setState(() {
-      _flag = e;
-    });
-  }
-
-  Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.all(50.0),
-        child: Column(
-          children: <Widget>[
-            Center(
-              child: new Icon(
-                Icons.thumb_up,
-                color: _flag ? Colors.orange[700] : Colors.grey[500],
-                size: 100.0,
-              ),
-            ),
-            new Checkbox(
-              activeColor: Colors.blue,
-              value: _flag,
-              onChanged: _handleCheckbox,
-            ),
-          ],
-        ));
-  }
+  KeyValueFavorites({this.key, this.isChecked});
 }
