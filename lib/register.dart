ceac7bfe2bfea9f416082dfc7749920d379bf58d
takumi34 <1shiranui@gmail.com>
2019-12-11 21:14:56 +0900

    update: register.dart. enables checkbox

--- lib/register.dart
+++ lib/register.dart
@@ -7,25 +7,14 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-  bool _isChecked = false;
-
-// TODO 今は決め打ちでリストの中身が入ってるので可変にする
+// TODO 今は決め打ちでリストの中身が入ってるので可変にする?
   List<KeyValueFavorites> _datas = [
     KeyValueFavorites(key: "桜", isChecked: false),
     KeyValueFavorites(key: "バラ", isChecked: false),
     KeyValueFavorites(key: "菊", isChecked: false),
     KeyValueFavorites(key: "紅葉", isChecked: false),
-
   ];
 
-
-
-  List<String> _texts = [
-    "桜","菊","バラ","ビル",
-  ];
-
-
-  //TODO とりあえず今は_textsで表示している。あとで_datasに差し替え。現状の_textsだとkeyがないので、checkboxが全て連動している状態
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,16 +23,17 @@ class _RegisterState extends State<Register> {
       ),
       body: ListView(
         padding: EdgeInsets.all(10.0),
-        children: _texts.map((text) => CheckboxListTile(
-          title: Text(text),
-          value: _isChecked,
-          onChanged: (val) {
-            setState(() {
-              _isChecked = val;
-
-            });
-          },
-        )).toList(),
+        children: _datas
+            .map((text) => CheckboxListTile(
+                  title: Text(text.key),
+                  value: text.isChecked,
+                  onChanged: (val) {
+                    setState(() {
+                      text.isChecked = val;
+                    });
+                  },
+                ))
+            .toList(),
       ),
     );
   }
