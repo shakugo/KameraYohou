e3992805216361bb52e1d30b7f2176af4bf6e192
Keisuke Fukuda <comet.helping@gmail.com>
2020-01-22 00:07:39 +0900

    #37:現在地取得関数の追加

---
+++ lib/currentLocation.dart
@@ -0,0 +1,10 @@
+import 'package:geolocator/geolocator.dart';
+
+class CurrentLocation {
+  void getCurrentLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print("current position!!!!!");
+    print(position);
+  }
+}
