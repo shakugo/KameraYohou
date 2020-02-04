2f480ffd37f9e0674e18df67c9d7df0812f49359
Takumi <34294333+takumi34@users.noreply.github.com>
2020-02-04 23:11:41 +0900

    fix typo

--- lib/currentLocation.dart
+++ lib/currentLocation.dart
@@ -4,6 +4,6 @@ class CurrentLocation {
   void getCurrentLocation() async {
     Position position = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    print("CurrentPosition:"position);
+    print(position);
   }
 }
