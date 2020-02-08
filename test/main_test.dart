f6aa15dd24845fc29856f5114c40490c13113bec
tonchan <cleartone1216@gmail.com>
2020-02-08 14:00:30 +0900

    add main test

---
+++ test/main_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kamera_yohou/main.dart';
+
+void main() {
+  testWidgets('Home App title', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    // Home is Spot List Widget
+    expect(find.text('Spot List'), findsOneWidget);
+  });
+}
