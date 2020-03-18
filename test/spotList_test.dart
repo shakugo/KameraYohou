2255956f29539b71135384145ef22b51f30adf11
tonchan <cleartone1216@gmail.com>
2020-03-18 22:12:51 +0900

    fix unit test

--- test/spotList_test.dart
+++ test/spotList_test.dart
@@ -10,12 +10,14 @@ void main() async {
   String widgetTitle = 'Spot List';
   group('UI', () {
     testWidgets('Spot List title', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: new SpotList(title: widgetTitle)));
+      await tester
+          .pumpWidget(MaterialApp(home: new SpotList(title: widgetTitle)));
       // Title shoud be Spot List
       expect(find.text(widgetTitle), findsOneWidget);
     });
     testWidgets('Open drawer menu', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: new SpotList(title: widgetTitle)));
+      await tester
+          .pumpWidget(MaterialApp(home: new SpotList(title: widgetTitle)));
       //Menu should be CLOSED when starting app
       expect(find.byType(Drawer), findsNothing);
 
@@ -37,12 +39,19 @@ void main() async {
       final spotList = SpotList(title: widgetTitle);
       when(mockClient.get(any, headers: anyNamed('headers'))).thenAnswer(
           (_) async =>
+              http.Response('{"Items":{"subjects":["aaa","bbb"]}}', 200));
+      when(mockClient.post(any,
+              headers: anyNamed('headers'), body: anyNamed('body')))
+          .thenAnswer((_) async =>
               http.Response('{"Items":[],"Count":0,"ScannedCount":0}', 200));
 
       spotList.httpClient = mockClient;
       await tester.pumpWidget(MaterialApp(home: spotList));
       await tester.pumpAndSettle();
       //verify 'a called count is 1'
+      verify(mockClient.post(any,
+              headers: anyNamed('headers'), body: anyNamed('body')))
+          .called(1);
       verify(mockClient.get(any, headers: anyNamed('headers'))).called(1);
     });
 
@@ -50,7 +59,11 @@ void main() async {
       final spotList = SpotList(title: widgetTitle);
       final mockClient = MockClient();
       when(mockClient.get(any, headers: anyNamed('headers'))).thenAnswer(
-          (_) async => http.Response(
+          (_) async =>
+              http.Response('{"Items":{"subjects":["aaa","bbb"]}}', 200));
+      when(mockClient.post(any,
+              headers: anyNamed('headers'), body: anyNamed('body')))
+          .thenAnswer((_) async => http.Response(
               '{"Items":[{"spot_id":"abcdefg","spot_name":"ABCDEFG"}],"Count":1,"ScannedCount":1}',
               200));
 
@@ -65,7 +78,10 @@ void main() async {
       final spotList = SpotList(title: widgetTitle);
       final mockClient = MockClient();
       when(mockClient.get(any, headers: anyNamed('headers'))).thenAnswer(
-          (_) async =>
+          (_) async => http.Response('{"Items":{"subjects":[]}}', 200));
+      when(mockClient.post(any,
+              headers: anyNamed('headers'), body: anyNamed('body')))
+          .thenAnswer((_) async =>
               http.Response('{"Items":[],"Count":0,"ScannedCount":0}', 200));
 
       spotList.httpClient = mockClient;
